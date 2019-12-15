b3d81d0f850463027f282c05c1b7036535deec1c
Bonvic Bundi <nyabuyabonvic@gmail.com>
2019-12-15 16:39:24 +0200

    setup files

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import './app.scss';
+
+class App extends Component {
+    state = {
+        counter: 0
+    };
+
+    handleClick = () => {
+        this.setState(prevState => {
+            return { counter: prevState.counter + 1 };
+        });
+    };
+    render() {
+        return (
+            <div className="App">
+                <h1>I'm configuring setting up webpack</h1>
+                <p>{`The count now is: ${this.state.counter}`}</p>
+                <button onClick={this.handleClick}>Click me</button>
+            </div>
+        );
+    }
+}
+
+export default App;
